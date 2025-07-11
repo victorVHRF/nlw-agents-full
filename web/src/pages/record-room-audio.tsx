bcaebe76c17310b42dbff16b485b1d5287d3333b
Victor Hugo Rosa Fonseca <victor.fonseca@tvmorena.com.br>
2025-07-11 16:52:48 -0400

    feat: implementado geracao de respostas com ia

--- web/src/pages/record-room-audio.tsx
+++ web/src/pages/record-room-audio.tsx
@@ -16,12 +16,17 @@ export function RecordRoomAudio() {
 
     const [isRecording, setIsRecording] = useState(false)
     const recorder = useRef<MediaRecorder | null>(null)
+    const intervalRef = useRef<NodeJS.Timeout>(null)
 
     async function stopRecording() {
         setIsRecording(false)
 
         if (recorder.current && recorder.current.state === 'inactive') {
             recorder.current.stop()
+        }
+
+        if (intervalRef.current) {
+            clearInterval(intervalRef.current)
         }
 
     }
@@ -41,22 +46,7 @@ export function RecordRoomAudio() {
 
     }
 
-    async function startRecording() {
-        if (!isRecordingSupported) {
-            alert('O seu navegador nao suporta gravacao')
-            return
-        }
-
-        setIsRecording(true)
-
-        const audio = await navigator.mediaDevices.getUserMedia({
-            audio: {
-                echoCancellation: true,
-                noiseSuppression: true,
-                sampleRate: 44_100,
-            }
-        })
-
+    function createRecorder(audio: MediaStream) {
         recorder.current = new MediaRecorder(audio, {
             mimeType: 'audio/webm',
             audioBitsPerSecond: 64_000
@@ -79,6 +69,31 @@ export function RecordRoomAudio() {
         }
 
         recorder.current.start()
+    }
+
+    async function startRecording() {
+        if (!isRecordingSupported) {
+            alert('O seu navegador nao suporta gravacao')
+            return
+        }
+
+        setIsRecording(true)
+
+        const audio = await navigator.mediaDevices.getUserMedia({
+            audio: {
+                echoCancellation: true,
+                noiseSuppression: true,
+                sampleRate: 44_100,
+            }
+        })
+
+        createRecorder(audio)
+
+        intervalRef.current = setInterval(() => {
+            recorder.current?.stop()
+
+            createRecorder(audio)
+        }, 5_000)
 
     }
 
